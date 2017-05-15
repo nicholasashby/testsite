39355e5beaa0d31e38258aaa814b9f86dcd0c166
Nick Ashby <nicholas.ashby@gmail.com>
2017-05-14 18:10:41 -0700

    firstcommit

---
+++ wp-content/themes/testsite/Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        files: {
+          'build/css/build.css' : 'sass/main.scss',
+        },
+      },
+    },
+    concat: {
+      options: {
+        separator: ';',
+      },
+      banner: 
+        '// ***********************\
+        // * zrrdigitalmedia.com *\
+        // ***********************\
+        \
+        \
+        ',
+      dist: {
+        src: ['js_vendor/jquery-1.12.4.min.js', 'js/**/*.js'],
+        dest: 'build/js/build.js',
+      },
+    },
+    watch: {
+      sass: {
+        files: ['sass/**/*.scss'],
+        tasks: ['sass'],
+        options: {
+          livereload : 35729
+        },
+      },
+      js: {
+        files: ['js/**/*.js'],
+        tasks: ['concat'],
+        options: {
+          livereload : 35729
+        },
+      },
+      php: {
+        files: ['**/*.php'],
+        options: {
+          livereload : 35729
+        },
+      },
+      options: {
+        style: 'expanded',
+        compass: true,
+      },
+    },
+  });
+
+  
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['sass', 'concat', 'watch']);
+ 
+
+};
---
+++ wp-content/themes/testsite/index.php
@@ -0,0 +1,8 @@
+<?php 
+
+get_header();
+
+echo 'zrrdigitalmedia theme banana';
+
+get_footer();
+?>
